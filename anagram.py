86b295054fab1bae5267d96caf3c5fe7150de923
Amit Samra <asamra@zipcodes-MacBook-Pro.local>
2020-03-05 20:32:15 -0500

    Exercise 3

--- anagram.py
+++ anagram.py
@@ -2,5 +2,10 @@ def is_anagram(first_string: str, second_string: str) -> bool:
     """
     Given two strings, this functions determines if they are an anagram of one another.
     """
-    pass  # remove pass statement and implement me
 
+	a = first_string.split()
+	#print(a)
+	b = second_string.split()
+	#print(b)
+	return a == b
+	
